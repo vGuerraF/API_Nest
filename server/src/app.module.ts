aacea93fce665fee6c44f40394cb818c44e9a8cd
vGuerraF <viniciusguerraf@gmail.com>
2023-02-20 14:18:52 -0300

    feat: base structure of crud

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { UserController } from './user/user.controller';
 import { UserService } from './user/services/user.service';
 import { DatabaseModule } from './prisma/database.module';
 import { UserRepository } from './user/user.repository';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ProfileModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
---
+++ server/src/profile/dto/create-profile.dto.ts
@@ -0,0 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
+export class CreateProfileDto {
+    name: string;
+    image: string;
+    
+}
